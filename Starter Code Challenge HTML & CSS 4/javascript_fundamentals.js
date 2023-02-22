2c406d271ebd84159f8b84ef13bda1faec85414c
jimmyx27 <75764910+jimmyx27@users.noreply.github.com>
2023-02-22 13:35:23 -0500

    Update javascript_fundamentals.js

--- Starter Code Challenge HTML & CSS 4/javascript_fundamentals.js
+++ Starter Code Challenge HTML & CSS 4/javascript_fundamentals.js
@@ -28,14 +28,19 @@ Arrays & Loops
 Step 1
 Create an empty array and assign it to a variable called "numberList".
 */
-
+let numberList = [];
 /*
 Step 2
 Using a for loop, place the numbers from 0 to 5 into the "numberList" array.
 console.log the numberList array.
 */
-
+for (let i = 0; i <=5; i++) {
+  numberList.push(i);
+}
+console.log(numberList);
 /*
 Step 3
 Remove the last number in the array and console.log the array.
+numberList.pop();
+console.log(numberList);
 */
